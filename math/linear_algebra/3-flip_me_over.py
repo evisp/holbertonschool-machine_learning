8ffd5832e6ecc2db3cb215258ab0121c7a2e1eea
evisp <evisplaku@gmail.com>
2024-01-22 13:35:20 +0000

    add: task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 """ defines function that transposes a 2D matrix """
 
-"""
-def matrix_transpose(matrix):
-    # Returns the transpose of the given 2D matrix.
-    matrix_transpose = []
-
-    for row in matrix:
-        for idx, value in enumerate(row):
-            if len(matrix_transpose) <= idx:
-                matrix_transpose.append([])
-            matrix_transpose[idx].append(value)
-
-    return matrix_transpose
-"""
 
 def matrix_transpose(matrix):
+    """Returns the transpose of the given 2D matrix."""
     rows = len(matrix)
     cols = len(matrix[0]) if matrix else 0
 
