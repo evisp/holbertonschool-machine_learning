f3e5cf7afe39a26c31c9f33b064cf358d6287553
evisp <evisplaku@gmail.com>
2024-01-22 13:38:56 +0000

    update: task 5 without list comprehension

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,4 +6,13 @@ def add_matrices2D(mat1, mat2):
     """Returns a new matrix, the sum of two 2D matrices added element-wise."""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+
+    return result
+
