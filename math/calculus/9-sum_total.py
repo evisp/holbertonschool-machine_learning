d8729a667e10856fc93632ff0f2256ec21836673
evisp <evisplaku@gmail.com>
2024-02-06 12:53:50 +0000

    add task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,5 +5,6 @@ A script that calculates the sum of squares
 
 
 def summation_i_squared(n):
+    """ a function that calculates the sum of squares"""
     sum = (n*(n+1)*(2*n+1)) / 6
-    return sum 
+    return sum
