c4f3e38460f3c523a37bc9df9c3a2bb345e781fc
evisp <evisplaku@gmail.com>
2024-02-13 12:32:57 +0000

    add: methods for pdf and cdf of normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -41,3 +41,26 @@ class Normal:
         stddev = self.stddev
         x = (z * stddev) + mean
         return x
+    
+     def pdf(self, x):
+        """calculates the value of the PDF for a given x-value """
+        mean = self.mean
+        stddev = self.stddev
+        e = 2.7182818285
+        pi = 3.1415926536
+        power = -0.5 * (self.z_score(x) ** 2)
+        coefficient = 1 / (stddev * ((2 * pi) ** (1 / 2)))
+        pdf = coefficient * (e ** power)
+        return pdf
+
+    def cdf(self, x):
+        """calculates the value of the CDF for a given x-value """
+        mean = self.mean
+        stddev = self.stddev
+        pi = 3.1415926536
+        value = (x - mean) / (stddev * (2 ** (1 / 2)))
+        erf = value - ((value ** 3) / 3) + ((value ** 5) / 10)
+        erf = erf - ((value ** 7) / 42) + ((value ** 9) / 216)
+        erf *= (2 / (pi ** (1 / 2)))
+        cdf = (1 / 2) * (1 + erf)
+        return cdf
