d08d973999c7c511aeb86f773237c06f91dd4f33
evisp <evisplaku@gmail.com>
2024-03-25 10:53:02 +0000

    method to calculate the cost function

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Neuron class that defines a single neuron performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    A single neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        get method for the private instance attribute __W
+        """
+        return (self.__W)
+
+    @property
+    def b(self):
+        """
+        get method for the private instance attribute __b
+        """
+        return (self.__b)
+
+    @property
+    def A(self):
+        """
+        get method for the private instance attribute _A
+        """
+        return (self.__A)
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neuron
+        """
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
