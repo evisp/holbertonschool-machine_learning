93e528258c972be9b75e2e777fb7ed71aabdf729
evisp <evisplaku@gmail.com>
2024-03-26 11:41:53 +0000

    add: task 8

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -27,9 +27,10 @@ class NeuralNetwork:
             raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = 0
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
+
 
