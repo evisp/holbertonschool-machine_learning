c0f6f946b6e85bbdc5d4b35d8f7ba7c1ba7856e6
evisp <evisplaku@gmail.com>
2024-03-26 11:47:22 +0000

    add:task 9

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Class NeuralNetwork that defines a neural network 
+Class NeuralNetwork that defines a neural network
 with one hidden layer performing binary classification
 """
 
@@ -26,11 +26,40 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
+    @property 
+    def W1(self):
+        """ get method for property W1"""
+        return self.__W1
+
+    @property 
+    def b1(self):
+        """ get method for property b1"""
+        return self.__b1
+
+    @property 
+    def A1(self):
+        """ get method for property A1"""
+        return self.__A1
+
+    @property 
+    def W2(self):
+        """ get method for property W2"""
+        return self.__W2
+
+    @property 
+    def b2(self):
+        """ get method for property b2"""
+        return self.__b2
+
+    @property 
+    def A2(self):
+        """ get method for property A2"""
+        return self.__A2
 
---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Class NeuralNetwork that defines a neural network
+with one hidden layer performing binary classification
+"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    A single neuron performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
