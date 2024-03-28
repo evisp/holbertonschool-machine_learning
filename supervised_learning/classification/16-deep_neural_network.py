7bd8d7db355b5e44ce5be368d7823146fcbeb20c
evisp <evisplaku@gmail.com>
2024-03-28 12:58:50 +0000

    add: task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,41 +1,28 @@
-
-#!/usr/bin/env python3
-"""
-Class that defines a deep neural network performing binary classification
-"""
-
-
 import numpy as np
-
 
 class DeepNeuralNetwork:
     """
-    Class that represents a deep neural network
+    Class that represents a deep neural network for binary classification
     """
 
     def __init__(self, nx, layers):
         """
-        class constructor
+        Class constructor
         """
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) < 1:
+        if not isinstance(layers, list) or not all(isinstance(layer, int) and layer > 0 for layer in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        weights = {}
+        self.cache = {}
+        self.weights = {}
         previous = nx
 
-        for index, layer in enumerate(layers, 1):
-            if type(layer) is not int or layer < 0:
-                raise TypeError("layers must be a list of positive integers")
-            weights["b{}".format(index)] = np.zeros((layer, 1))
-            weights["W{}".format(index)] = (
-                np.random.randn(layer, previous) * np.sqrt(2 / previous))
-            previous = layer
+        for l, layer_size in enumerate(layers, 1):
+            self.weights[f"W{l}"] = np.random.randn(layer_size, previous) * np.sqrt(2 / previous)
+            self.weights[f"b{l}"] = np.zeros((layer_size, 1))
+            previous = layer_size
 
         self.L = len(layers)
-        self.cache = {}
-        self.weights = weights
-
