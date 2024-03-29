215dff275d681a839c713945bc2e7e084161673f
evisp <evisplaku@gmail.com>
2024-03-29 10:57:47 +0000

    add task 28

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-DeepNeuralNetwork performing binary classification
+    Class DeepNeuralNetwork : deep NN performing binary classification
 """
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,8 +10,9 @@ import pickle
 
 class DeepNeuralNetwork:
     """
-    class that represents a deep neural network
+        class DeepNeuralNetwork
     """
+
     def __init__(self, nx, layers, activation='sig'):
         """
             class constructor
@@ -54,27 +54,42 @@ class DeepNeuralNetwork:
                      * np.sqrt(2 / layers[i - 1]))
             self.__weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
 
-    
     @property
     def L(self):
         """
-        gets the private instance attribute __L
+            The number of layers in neural network
+
+            :return: value for private attribute __L
         """
-        return (self.__L)
+        return self.__L
 
     @property
     def cache(self):
         """
-        gets the private instance attribute __cache
+            Dictionary to hold all intermediary value
+            Upon instantiation, empty
+
+            :return: value for private attribute __cache
         """
-        return (self.__cache)
+        return self.__cache
 
     @property
     def weights(self):
         """
-        gets the private instance attribute __weights
+            Dictionary hold all weights and biased of network
+
+            :return: value for private attribute __weights
         """
-        return (self.__weights)
+        return self.__weights
+
+    @property
+    def activation(self):
+        """
+            activation function used in the hidden layer
+
+            :return: value for private attribute __activation
+        """
+        return self.__activation
 
     def forward_prop(self, X):
         """
@@ -108,29 +123,41 @@ class DeepNeuralNetwork:
 
         return A, self.__cache
 
+    def cost(self, Y, A):
+        """
+            Calculate cross-entropy cost for multiclass
+
+            :param Y: ndarray, shape(1,m) correct labels
+            :param A: ndarray, shape(1,m) activated output
+
+            :return: cost
+        """
+
+        # store m value
+        m = Y.shape[1]
+
+        # calculate log loss function
+        log_loss = -(1 / m) * np.sum(Y * np.log(A))
+
+        return log_loss
+
     def evaluate(self, X, Y):
         """
             Method to evaluate the network's prediction
 
             :param X: ndarray shape(nx,m) contains input data
-            :param Y: ndarray shape (1,m) correct labels
+            :param Y: one-hot ndarray shape (classes,m)
 
             :return: network's prediction and cost of the network
         """
 
         # run forward propagation
-        output, cache = self.forward_prop(X)
+        A, _ = self.forward_prop(X)
 
         # calculate cost
-        cost = self.cost(Y, output)
+        cost = self.cost(Y, A)
 
-        # convert predicted proba to one-hot
-        result = np.zeros_like(output)
-
-        # label values
-        result[np.argmax(output, axis=0), np.arange(output.shape[1])] = 1
-
-        return result, cost
+        return np.where(A == np.max(A, axis=0), 1, 0), cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """
@@ -172,72 +199,107 @@ class DeepNeuralNetwork:
     def train(self, X, Y, iterations=5000, alpha=0.05,
               verbose=True, graph=True, step=100):
         """
-        trains the neuron and updates __weights and __cache
+            Method to train deep neural network
+
+            :param X: ndarray, shape(nx,m), input data
+            :param Y: ndarray, shapte(1,m), correct labels
+            :param iterations: number of iterations to train over
+            :param alpha: learning rate
+            :param verbose: boolean print or not information
+            :param graph: boolean print or not graph
+            :param step: int
+
+            :return: evaluation of training after iterations
         """
-        if type(iterations) is not int:
+
+        if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) is not float:
+        if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        if verbose or graph:
-            if type(step) is not int:
+        if not isinstance(verbose, bool):
+            raise TypeError("verbose must be a boolean")
+        if not isinstance(graph, bool):
+            raise TypeError("graph must be a boolean")
+        if verbose is True or graph is True:
+            if not isinstance(step, int):
                 raise TypeError("step must be an integer")
             if step <= 0 or step > iterations:
                 raise ValueError("step must be positive and <= iterations")
+
+        # list to store cost /iter
         if graph:
-            import matplotlib.pyplot as plt
-            x_points = np.arange(0, iterations + 1, step)
-            points = []
-        for itr in range(iterations):
-            A, cache = self.forward_prop(X)
-            if verbose and (itr % step) == 0:
-                cost = self.cost(Y, A)
-                print("Cost after " + str(itr) + " iterations: " + str(cost))
-            if graph and (itr % step) == 0:
-                cost = self.cost(Y, A)
-                points.append(cost)
-            self.gradient_descent(Y, cache, alpha)
-        itr += 1
-        if verbose:
-            cost = self.cost(Y, A)
-            print("Cost after " + str(itr) + " iterations: " + str(cost))
+            costs = []
+            count = []
+
+        for i in range(iterations + 1):
+            # run forward propagation
+            A, _ = self.forward_prop(X)
+
+            # run gradient descent for all iterations except the last one
+            if i != iterations:
+                self.gradient_descent(Y, self.cache, alpha)
+
+            if i % step == 0 or i == iterations:
+                current_cost = self.cost(Y, A)
+                if verbose:
+                    print("Cost after {} iterations: {}".
+                          format(i, current_cost))
+                if graph:
+                    costs.append(current_cost)
+                    count.append(i)
+
+            # verbose TRUE, every step + first and last iteration
+            if verbose and (i % step == 0 or i == 0 or i == iterations):
+                # run evaluate
+                print("Cost after {} iterations: {}".format(i, current_cost))
+
+        # graph TRUE after training complete
         if graph:
-            cost = self.cost(Y, A)
-            points.append(cost)
-            y_points = np.asarray(points)
-            plt.plot(x_points, y_points, 'b')
-            plt.xlabel("iteration")
-            plt.ylabel("cost")
-            plt.title("Training Cost")
+            plt.plot(count, costs, 'b-')
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
             plt.show()
-        return (self.evaluate(X, Y))
+
+        return self.evaluate(X, Y)
 
     def save(self, filename):
         """
-        saves the instance object to a file in pickle format
-        """
-        if not isinstance(filename, str):
-            print("Error: filename must be a string.")
-            return
+            Method to saves instance object to a file in pickle format
 
+            :param filename: file which the object should be saved
+
+        """
+        # test extention
         if not filename.endswith('.pkl'):
             filename += '.pkl'
 
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
+        # open file in binary write mode
+        with open(filename, 'wb') as file:
+            # use pickel to dump the object into the file
+            pickle.dump(self, file)
 
     @staticmethod
     def load(filename):
         """
-        loads a pickled DeepNeuralNetwork object from a file
+            method to load a pickled DeepNeuralNetwork object
+
+            :param filename: file from which object should be loaded
+
+            :return: loaded object
+                    or None if filename doesn't exist
         """
-        import pickle
+
         try:
-            with open(filename, 'rb') as f:
-                obj = pickle.load(f)
-                return obj
+            # open file in binary mode
+            with open(filename, 'rb') as file:
+                # use pickle to load
+                loaded_object = pickle.load(file)
+            return loaded_object
+
         except FileNotFoundError:
             return None
