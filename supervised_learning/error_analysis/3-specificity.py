903c1091a85fd4e0063aad047a8eeffa501e1eda
evisp <evisplaku@gmail.com>
2024-04-22 09:44:57 +0000

    method to calcualte specifity

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    Specificity
+"""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """
+        Function to calculates specificity of each class
+        in a confusion matrix
+
+        :param confusion: ndarray, shape(classes,classes) confusion matrix
+
+        :return: ndarray, shape(classes,), specificity of each class
+    """
+    # number of classes
+    classes = confusion.shape[0]
+    # initialize specificity
+    specificity_matrix = np.zeros((classes,))
+
+    # formule specificity : true negative / (true negative + false positive)
+    for i in range(classes):
+        true_pos = confusion[i, i]
+        false_pos = np.sum(confusion[:, i]) - true_pos
+        false_neg = np.sum(confusion[i, :]) - true_pos
+
+        # TP = Total - (TP + FP + FN)
+        true_neg = np.sum(confusion) - (true_pos + false_pos + false_neg)
+
+        specificity_matrix[i] = true_neg / (true_neg + false_pos)
+
+    return specificity_matrix
