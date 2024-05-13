1f75916864866cca2ca33f318253b455b94c05e6
evisp <evisplaku@gmail.com>
2024-05-13 08:37:46 +0000

    fix pycode style

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -9,64 +9,43 @@ import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
-                     epochs=5, load_path="/tmp/model.ckpt",
-                     save_path="/tmp/model.ckpt"):
+def create_mini_batches(X, Y, batch_size):
     """
-    Trains a loaded neural network model using mini-batch gradient descent
+    Creates mini-batches to be used for training
+    a neural network using mini-batch gradient descent.
 
+    Args:
+        X (numpy.ndarray): Input data of shape (m, nx).
+        Y (numpy.ndarray): Labels of shape (m, ny).
+        batch_size (int): Number of data points in a batch.
+
+    Returns:
+        list: List of mini-batches containing tuples (X_batch, Y_batch).
     """
-    m = X_train.shape[0]
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(load_path + '.meta')
-        saver.restore(sess, load_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
+    m = X.shape[0]
+    mini_batches = []
 
-        for epoch in range(epochs + 1):
-            print("After {} epochs:".format(epoch))
-            train_cost = sess.run(loss, feed_dict={x: X_train,
-                                                   y: Y_train})
-            print("\tTraining Cost: {}".format(train_cost))
-            train_accuracy = sess.run(accuracy, feed_dict={x: X_train,
-                                                           y: Y_train})
-            print("\tTraining Accuracy: {}".format(train_accuracy))
-            valid_cost = sess.run(loss, feed_dict={x: X_valid,
-                                                   y: Y_valid})
-            print("\tValidation Cost: {}".format(valid_cost))
-            valid_accuracy = sess.run(accuracy, feed_dict={x: X_valid,
-                                                           y: Y_valid})
-            print("\tValidation Accuracy: {}".format(valid_accuracy))
-            if epoch == epochs:
-                break
-            X_train_s, Y_train_s = shuffle_data(X_train, Y_train)
-            if (m % batch_size) is 0:
-                mini_batch_total = m // batch_size
-            else:
-                mini_batch_total = (m // batch_size) + 1
+    # Shuffle the data
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
 
-            step_number = 0
-            for mini_batch in range(mini_batch_total):
-                low = mini_batch * batch_size
-                high = ((mini_batch + 1) * batch_size)
-                if high > m:
-                    high = m
-                sess.run(train_op, feed_dict={x: X_train_s[low:high, :],
-                                              y: Y_train_s[low:high, :]})
-                step_number += 1
-                if (step_number % 100) is 0:
-                    print("\tStep {}:".format(step_number))
-                    step_cost = sess.run(
-                        loss,
-                        feed_dict={x: X_train_s[low:high, :],
-                                   y: Y_train_s[low:high, :]})
-                    print("\t\tCost: {}".format(step_cost))
-                    step_accuracy = sess.run(
-                        accuracy,
-                        feed_dict={x: X_train_s[low:high, :],
-                                   y: Y_train_s[low:high, :]})
-                    print("\t\tAccuracy: {}".format(step_accuracy))
-        return (saver.save(sess, save_path))
+    # Calculate the number of batches
+    num_batches = m // batch_size
+
+    for i in range(num_batches):
+        start_idx = i * batch_size
+        end_idx = start_idx + batch_size
+
+        X_batch = X_shuffled[start_idx:end_idx]
+        Y_batch = Y_shuffled[start_idx:end_idx]
+
+        mini_batches.append((X_batch, Y_batch))
+
+    # Handle the last batch
+    # (if the total number of data points is not divisible by batch_size)
+    if m % batch_size != 0:
+        start_idx = num_batches * batch_size
+        X_batch = X_shuffled[start_idx:]
+        Y_batch = Y_shuffled[start_idx:]
+        mini_batches.append((X_batch, Y_batch))
+
+    return mini_batches
