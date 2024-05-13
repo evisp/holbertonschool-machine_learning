ba2c783ab3666d47806b9204b38ba216b7eb1b2f
evisp <evisplaku@gmail.com>
2024-05-13 08:19:54 +0000

    update RMSProp implementaion for tensorflow v2

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -18,5 +18,8 @@ def create_RMSProp_op(alpha, beta2, epsilon):
     Returns:
         optimizer: Optimizer object for RMSProp.
     """
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha, rho=beta2, epsilon=epsilon)
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon)
     return optimizer
