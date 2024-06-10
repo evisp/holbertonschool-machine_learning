df0f6101d4b7f42fc39d9d92d7554bafdd472e0e
evisp <evisplaku@gmail.com>
2024-06-10 11:23:35 +0200

    update task on dense block

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -1,47 +1,33 @@
 #!/usr/bin/env python3
-"""Dense Block Module"""
-
-import tensorflow.keras as K
+"""
+Task 5
+"""
+from tensorflow import keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
-    """Builds a Dense Block.
-
-    Args:
-        X: the output from the previous layer.
-
-        nb_filters: an integer representing the number of filters in X.
-
-        growth_rate: the growth rate for the dense block.
-
-        layers: the number of layers in the dense block.
-
-    Returns:
-        The concatenated output of each layer within the Dense Block and the
-        number of filters within the concatenated outputs, respectively.
     """
-
-    concatenated_layers = [X]
-    current_filters = nb_filters
-    init = K.initializers.he_normal
+    """
+    he_normal = K.initializers.he_normal(seed=0)
+    H = X
 
     for _ in range(layers):
-        X = K.layers.BatchNormalization()(X)
-        X = K.layers.Activation("relu")(X)
-        X = K.layers.Conv2D(4 * growth_rate, 1,
-                            padding="same", kernel_initializer=init)(X)
+        # 1x1 conv
+        X = K.layers.BatchNormalization(axis=3)(H)
+        X = K.layers.Activation('relu')(X)
+        X = K.layers.Conv2D(growth_rate * 4, (1, 1),
+                            padding='same',
+                            kernel_initializer=he_normal)(X)
 
-        X = K.layers.BatchNormalization()(X)
-        X = K.layers.Activation("relu")(X)
-        X = K.layers.Conv2D(growth_rate, 3, padding="same",
-                            kernel_initializer=init)(X)
+        # 3x3 conv
+        X = K.layers.BatchNormalization(axis=3)(X)
+        X = K.layers.Activation('relu')(X)
+        X = K.layers.Conv2D(growth_rate, (3, 3),
+                            padding='same',
+                            kernel_initializer=he_normal)(X)
 
-        concatenated_layers.append(X)
-        X = K.layers.Concatenate()(concatenated_layers)
-        concatenated_layers.pop(0)
-        concatenated_layers.pop(0)
-        concatenated_layers.append(X)
+        # concatenate all outputs of dense block
+        H = K.layers.Concatenate(axis=3)([H, X])
+        nb_filters += growth_rate
 
-        current_filters += growth_rate
-
-    return X, current_filters
+    return H, nb_filters
