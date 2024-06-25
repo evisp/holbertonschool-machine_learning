2144bf52b7c821fed4834c4a7ce30a097b8b860f
evisp <evisplaku@gmail.com>
2024-06-25 18:55:55 +0000

    add: yolo algorithm constructor

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""Yolo Class Module"""
+"""
+Initialize Yolo
+"""
 
-import tensorflow.keras as K
+
+from tensorflow import keras as K
 
 
-class Yolo():
-    """class Yolo that uses the Yolo v3 algorithm to perform object detection
+class Yolo:
     """
-
+    Yolo class
+    """
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r', encoding='utf-8') as f:
-            self.class_names = [line.strip() for line in f]
+        with open(classes_path, 'r') as classes_file:
+            self.class_names = [
+                line.strip() for line in classes_file.readlines()
+                ]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
