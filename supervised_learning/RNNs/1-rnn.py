e58e816236ef551e35801fdde85f7320dabc20a3
evisp <evisplaku@gmail.com>
2024-07-08 06:52:22 +0000

    unction def rnn(rnn_cell, X, h_0): that performs forward propagation for a simple RNN

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""RNN Module"""
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+        Performs forward propagation for a simple RNN:
+    """
+
+    H = np.zeros((X.shape[0] + 1, h_0.shape[0], h_0.shape[1]))
+    H[0] = h_0
+
+    Y = np.zeros((X.shape[0], X.shape[1], rnn_cell.by.shape[1]))
+
+    for i, x_t in enumerate(X):
+        H[i + 1], Y[i] = rnn_cell.forward(H[i], x_t)
+
+    return H, Y
