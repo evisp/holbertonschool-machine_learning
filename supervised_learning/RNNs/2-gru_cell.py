54322752ac5d8d0236f2e88bbf2c302f2076564f
evisp <evisplaku@gmail.com>
2024-07-08 06:55:05 +0000

    add: functions for GRU adn LSTM cell

---
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""GRU Cell Module"""
+import numpy as np
+
+
+class GRUCell:
+    """
+        Represents a gated recurrent unit
+    """
+
+    def __init__(self, i, h, o):
+        
+        self.Wz = np.random.normal(size=(i+h, h))
+        self.Wr = np.random.normal(size=(i+h, h))
+        self.Wh = np.random.normal(size=(i+h, h))
+        self.Wy = np.random.normal(size=(h, o))
+        
+        self.bz = np.zeros((1, h))
+        self.br = np.zeros((1, h))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+            Performs forward propagation for one time step
+        """
+
+        def softmax(x):
+            """Compute softmax activation function"""
+            return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+
+        r_t = 1 / (1 + np.exp(-(np.dot(h_x, self.Wr) + self.br)))
+
+        z_t = 1 / (1 + np.exp(-(np.dot(h_x, self.Wz) + self.bz)))
+
+        rh_x = np.concatenate((r_t * h_prev, x_t), axis=1)
+
+        h_tilde = np.tanh(np.dot(rh_x, self.Wh) + self.bh)
+        h_next = (1 - z_t) * h_prev + z_t * h_tilde
+
+        output_t = softmax(np.dot(h_next, self.Wy) + self.by)
+
+        return h_next, output_t
---
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""LSTM Cell Module"""
+import numpy as np
+
+
+class LSTMCell:
+    """
+        Represents an LSTM unit:
+    """
+
+    def __init__(self, i, h, o):
+
+        self.Wf = np.random.normal(size=(i+h, h))
+        self.Wu = np.random.normal(size=(i+h, h))
+        self.Wc = np.random.normal(size=(i+h, h))
+        self.Wo = np.random.normal(size=(i+h, h))
+        self.Wy = np.random.normal(size=(h, o))
+        
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, c_prev, x_t):
+        """
+            Performs forward propagation for one time step
+        """
+
+        def softmax(x):
+            """Compute softmax activation function"""
+            return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+        def sigmoid(x):
+            """Compute sigmoid activation function"""
+            return 1 / (1 + np.exp(-x))
+
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+
+        f_t = sigmoid(np.dot(h_x, self.Wf) + self.bf)
+
+        u_t = sigmoid(np.dot(h_x, self.Wu) + self.bu)
+        c_tilde = np.tanh(np.dot(h_x, self.Wc) + self.bc)
+
+        c_next = f_t * c_prev + u_t * c_tilde
+
+        output_t = sigmoid(np.dot(h_x, self.Wo) + self.bo)
+        h_next = output_t * np.tanh(c_next)
+
+        y = softmax(np.dot(h_next, self.Wy) + self.by)
+
+        return h_next, c_next, y
