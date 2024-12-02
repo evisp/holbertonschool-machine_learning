8a05a5c01217565eae79090ea7a083ee0cb2d69c
evisp <evisplaku@gmail.com>
2024-12-02 17:40:26 +0000

    add: task 9

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" a script that calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """ a function that calculates the derivative of a polynomial"""
+    
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,5 +4,7 @@
 
 def summation_i_squared(n):
     """ a function that calculates the sum of squares"""
+    if type(n) is not int or n < 1:
+        return None
     result = (n * (n + 1) * (2 * n + 1)) / 6
     return int(result)
