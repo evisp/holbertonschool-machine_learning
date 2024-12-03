f492be204f8ba3fb1d96264dfaef16bf992624c7
evisp <evisplaku@gmail.com>
2024-12-03 17:49:41 +0000

    add task 2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -36,3 +36,26 @@ class Poisson:
 
         # Compute PMF using the formula: P(k; λ) = (λ^k * e^(-λ)) / k!
         return (e**(-self.lambtha) * self.lambtha ** k ) / math.factorial(k)
+
+    def cdf(self, k):
+        """
+        Calculates the CDF for a given number of “successes”.
+
+        Args:
+            k (int or float): The number of “successes”.
+
+        Returns:
+            float: The CDF value for k.
+        """
+        if not isinstance(k, int):
+            try:
+                k = int(k)
+            except ValueError:
+                return 0
+        
+        if k < 0:
+            return 0
+
+        # Compute CDF as the sum of PMF values from 0 to k
+        cdf = sum(self.pmf(i) for i in range(k + 1))
+        return cdf
