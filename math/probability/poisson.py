303af579eabb7038df591f1c759d9a442b1bfb11
evisp <evisplaku@gmail.com>
2024-12-05 14:35:42 +0100

    udpate possion class

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,63 +1,46 @@
 #!/usr/bin/env python3
-"""  a class Poisson that represents a poisson distribution """
+"""Poisson class"""
+
+
+e = 2.7182818285
 
 
 class Poisson:
+    """Class Poisson"""
     def __init__(self, data=None, lambtha=1.):
-        """ represents a poisson distribution"""
+        """Class constructor"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
-    
+            self.lambtha = sum(data)/len(data)
+
     def pmf(self, k):
-        """
-        Calculates the PMF for a given number of “successes”.
-
-        Args:
-            k (int or float): The number of “successes”.
-
-        Returns:
-            float: The PMF value for k.
-        """
-        import math
-        if not isinstance(k, int):
+        """Calculates the PMF"""
+        if type(k) is not int:
             k = int(k)
-        
         if k < 0:
             return 0
-
-        e = 2.7182818285
-
-        # Compute PMF using the formula: P(k; λ) = (λ^k * e^(-λ)) / k!
-        return (e**(-self.lambtha) * self.lambtha ** k ) / math.factorial(k)
+        fact = 1
+        for i in range(1, k + 1):
+            fact = fact * i
+        return (e ** -self.lambtha * self.lambtha ** k) / fact
 
     def cdf(self, k):
-        """
-        Calculates the CDF for a given number of “successes”.
-
-        Args:
-            k (int or float): The number of “successes”.
-
-        Returns:
-            float: The CDF value for k.
-        """
-        import math
-        if not isinstance(k, int):
-            try:
-                k = int(k)
-            except ValueError:
-                return 0
-        
+        """Calculates the CDF"""
+        if k is not int:
+            k = int(k)
         if k < 0:
             return 0
-
-        # Compute CDF as the sum of PMF values from 0 to k
-        cdf = sum(self.pmf(i) for i in range(k + 1))
-        return cdf
+        cdf = 0
+        for i in range(0, k + 1):
+            fact = 1
+            for j in range(1, i + 1):
+                fact = fact * j
+            cdf += self.lambtha ** i / fact
+        return cdf * e ** -self.lambtha
