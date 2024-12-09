018a5c94943641a80f1995192bda70a893eb4294
evisp <evisplaku@gmail.com>
2024-12-09 09:31:23 +0000

    add: task 1: script that creates a pd.DataFrame from a dictionary:

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task 1: script that creates a pd.DataFrame from a dictionary:
+"""
+import pandas as pd
+
+
+d = {'First': [0, 0.5, 1, 1.5], 'Second': ['one', 'two', 'three', 'four']}
+df = pd.DataFrame(d, index=list('ABCD'))
