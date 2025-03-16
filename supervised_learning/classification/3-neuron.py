0cb49bf44ec877999faddedddc66bdb74dfe3620
evisp <evisplaku@gmail.com>
2025-03-16 19:03:12 +0000

    add: binary classification with single neuron

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -19,9 +19,9 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
